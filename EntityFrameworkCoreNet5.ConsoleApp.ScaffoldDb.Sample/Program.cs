0ab1208e73d9675022fddc924ce53bbda487f641
Matyáš Sládek <acc.sladek@gmail.com>
2021-07-16 23:38:34 +0200

    Add project files.

---
+++ EntityFrameworkCoreNet5.ConsoleApp.ScaffoldDb.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCoreNet5.ConsoleApp.ScaffoldDb.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkNet5.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkNet5.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore").LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information).EnableSensitiveDataLogging();
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+    }
+}
