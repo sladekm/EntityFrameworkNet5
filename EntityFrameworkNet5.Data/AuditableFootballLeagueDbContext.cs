8545604ac086eb70a9b1186190cd2cae2a62ecdf
Matyáš Sládek <acc.sladek@gmail.com>
2021-09-29 07:54:57 +0200

    Added full database auditing

--- EntityFrameworkNet5.Data/AuditableFootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/AuditableFootballLeagueDbContext.cs
@@ -10,9 +10,46 @@ namespace EntityFrameworkNet5.Data
 {
     public abstract class AuditableFootballLeagueDbContext : DbContext
     {
+        public DbSet<Audit> Audits { get; set; }
+
         public async Task<int> SaveChangesAsync(string username)
         {
-            var entries = ChangeTracker.Entries().Where(q => q.State == EntityState.Added || q.State == EntityState.Modified);
+            var auditEntries = OnBeforeSaveChanges(username);
+            var saveResult = await base.SaveChangesAsync();
+            if (auditEntries != null || auditEntries.Count > 0)
+            {
+                await OnAfterSaveChanges(auditEntries);
+            }
+
+            return saveResult;
+        }
+
+        private Task OnAfterSaveChanges(List<AuditEntry> auditEntries)
+        {
+            foreach (var auditEntry in auditEntries)
+            {
+                foreach (var prop in auditEntry.TemporaryProperties)
+                {
+                    if (prop.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[prop.Metadata.Name] = prop.CurrentValue;
+                    }
+                    else
+                    {
+                        auditEntry.NewValues[prop.Metadata.Name] = prop.CurrentValue;
+                    }
+                }
+
+                Audits.Add(auditEntry.ToAudit());
+            }
+
+            return SaveChangesAsync();
+        }
+
+        private List<AuditEntry> OnBeforeSaveChanges(string username)
+        {
+            var entries = ChangeTracker.Entries().Where(q => q.State == EntityState.Added || q.State == EntityState.Modified || q.State == EntityState.Deleted);
+            var auditEntries = new List<AuditEntry>();
 
             foreach (var entry in entries)
             {
@@ -25,9 +62,46 @@ namespace EntityFrameworkNet5.Data
                     auditableObject.CreatedDate = DateTime.Now;
                     auditableObject.CreatedBy = username;
                 }
+
+                var auditEntry = new AuditEntry(entry);
+                auditEntry.TableName = entry.Metadata.GetTableName();
+                auditEntry.Action = entry.State.ToString();
+                auditEntries.Add(auditEntry);
+
+                foreach (var property in entry.Properties)
+                {
+                    if(property.IsTemporary)
+                    {
+                        auditEntry.TemporaryProperties.Add(property);
+                        continue;
+                    }
+
+                    string propertyName = property.Metadata.Name;
+                    if (property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[propertyName] = property.CurrentValue;
+                        break;
+                    }
+
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            break;
+
+                        case EntityState.Deleted:
+                            auditEntry.OldValues[propertyName] = property.OriginalValue;
+                            break;
+                    }
+                }
             }
 
-            return await base.SaveChangesAsync();
+            foreach (var pendingAuditEntry in auditEntries.Where(q => q.HasTemporaryProperties == false))
+            {
+                Audits.Add(pendingAuditEntry.ToAudit());
+            }
+
+            return auditEntries.Where(q => q.HasTemporaryProperties).ToList();
         }
     }
 }
--- EntityFrameworkNet5.Data/Migrations/20210929055059_AddedAuditTable.Designer.cs
+++ EntityFrameworkNet5.Data/Migrations/20210929055059_AddedAuditTable.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkNet5.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkNet5.Data.Migrations
 {
     [DbContext(typeof(FootballLeagueDbContext))]
-    partial class FootballLeagueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210929055059_AddedAuditTable")]
+    partial class AddedAuditTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
